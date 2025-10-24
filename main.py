69ca540a9fc2c5f27a538c30458ed984e9b4042a
Kang-Jiho79 <giho2630@gmail.com>
2025-10-24 12:15:29 +0900

    main frame

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from pico2d import *
 
+def reset_world():
+    pass
+
+def handle_events():
+    pass
+
+
+def update_world():
+    pass
+
+
+def render_world():
+    pass
+
+
 running = True
 
 open_canvas()
 
+reset_world()
+
+
 while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
 
 
 close_canvas()
