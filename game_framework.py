87c2b9c1846389fc426940899b89dd7366b3183e
Kang-Jiho79 <giho2630@gmail.com>
2025-11-01 20:35:14 +0900

    add game_framework pop_mode

--- game_framework.py
+++ game_framework.py
@@ -15,3 +15,11 @@ def push_mode(mode):
         stack[-1].pause()
     stack.append(mode)
     mode.init()
+
+def pop_mode():
+    global stack
+    if (len(stack) > 0):
+        stack[-1].finish()
+        stack.pop()
+    if (len(stack) > 0):
+        stack[-1].resume()
