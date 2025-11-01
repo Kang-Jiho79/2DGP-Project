618d3e2bba5ad5b00bbb60395723e30eee6d8743
Kang-Jiho79 <giho2630@gmail.com>
2025-11-01 20:32:51 +0900

    add game_framework change_mode

--- game_framework.py
+++ game_framework.py
@@ -1,2 +1,10 @@
 running = None
 stack = None
+
+def change_mode(mode):
+    global stack
+    if (len(stack) > 0):
+        stack[-1].finish()
+        stack.pop()
+    stack.append(mode)
+    mode.init()
