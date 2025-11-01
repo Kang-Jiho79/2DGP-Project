b3cf9ac39ab3233a661181797334af80be127784
Kang-Jiho79 <giho2630@gmail.com>
2025-11-01 20:35:48 +0900

    add game_framework run

--- game_framework.py
+++ game_framework.py
@@ -27,3 +27,18 @@ def pop_mode():
 def quit():
     global running
     running = False
+
+def run(start_mode):
+    global running, stack
+    running = True
+    stack = [start_mode]
+    start_mode.init()
+
+    while running:
+        stack[-1].handle_events()
+        stack[-1].update()
+        stack[-1].draw()
+
+    while (len(stack) > 0):
+        stack[-1].finish()
+        stack.pop()
