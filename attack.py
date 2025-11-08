fcc3c98a7411590502b178580d45f41991b55f31
Kang-Jiho79 <giho2630@gmail.com>
2025-11-08 19:21:40 +0900

    attack 속도 frame_time에 맞춰서 속도 조정

--- attack.py
+++ attack.py
@@ -6,6 +6,10 @@ player_attack_animation = (
     (0,0,42,33), (42,0,40,33), (82,0,40,33), (122,0,40,33)
 )
 
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
 class Attack:
     image = None
 
@@ -16,9 +20,15 @@ class Attack:
         self.face_dir = face_dir
         self.frame = 0
         self.player = player
-    
+
+    def update(self):
+        self.frame = self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time
+        if self.frame >= len(player_attack_animation):
+            game_world.remove_object(self)
+            self.player.end_attack()
+
     def draw(self):
-        frame_data = player_attack_animation[self.frame]
+        frame_data = player_attack_animation[int(self.frame)]
 
         # 방향에 따른 오프셋과 회전 각도 설정
         offset_distance = 30  # 플레이어로부터 떨어뜨릴 거리
@@ -44,15 +54,10 @@ class Attack:
         if self.face_dir == 3:  # left
             self.image.clip_composite_draw(frame_data[0], frame_data[1], frame_data[2], frame_data[3],
                                                          angle, 'h',
-                                                         effect_x, effect_y, 50,
-                                                         100)
+                                                         effect_x, effect_y, 25,
+                                                         50)
         else:
             self.image.clip_composite_draw(frame_data[0], frame_data[1], frame_data[2], frame_data[3],
                                                          angle, '',
-                                                         effect_x, effect_y, 50,
-                                                         100)
-    def update(self):
-        self.frame += 1
-        if self.frame >= len(player_attack_animation):
-            game_world.remove_object(self)
-            self.player.end_attack()
+                                                         effect_x, effect_y, 25,
+                                                         50)
