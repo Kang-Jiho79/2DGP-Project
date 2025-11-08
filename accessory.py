33adf62473abd8302c3044f78533945cdd3ae1fa
Kang-Jiho79 <giho2630@gmail.com>
2025-11-08 20:38:24 +0900

    accessory 객체 init 추가

---
+++ accessory.py
@@ -0,0 +1,6 @@
+class Accessory:
+    def __init__(self, name, effect_type, effect_value):
+        self.name = name
+        self.effect_type = effect_type
+        self.effect_value = effect_value
+        self.equipped = False
