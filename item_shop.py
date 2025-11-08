ba8e3858bf22d31a3c01844d48b9425e5c6418da
Kang-Jiho79 <giho2630@gmail.com>
2025-11-09 01:21:45 +0900

    item_shop 객체 생성

--- item_shop.py
+++ item_shop.py
@@ -1,1 +1,11 @@
 from pico2d import *
+
+class ItemShop:
+    def __init__(self):
+        self.image = load_image('resource/npc/item_npc_ui.png')
+
+    def draw(self):
+        self.image.clip_composite_draw(0, 0, 1472, 704, 0, '', 400, 300, 800, 600)
+
+    def update(self):
+        pass
