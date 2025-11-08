64a7012c9913caf6865028bba46c9108527b51f2
Kang-Jiho79 <giho2630@gmail.com>
2025-11-08 18:46:02 +0900

    attack state에서 객체로 변경 - Attack init 추가

--- attack.py
+++ attack.py
@@ -3,4 +3,11 @@ import game_world
 import game_framework
 
 class Attack:
-    pass
+    image = None
+
+    def __init__(self, x=400, y=300, face_dir=1):
+        if Attack.image == None:
+            Attack.image = load_image('resource/player/player_attack.png')
+        self.x, self.y = x, y
+        self.face_dir = face_dir
+        self.frame = 0
