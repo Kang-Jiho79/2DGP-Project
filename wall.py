48c31e8eb6810116e8e590b4066c0a8aaaf9d6e3
Kang-Jiho79 <giho2630@gmail.com>
2025-11-25 19:31:42 +0900

    벽 객체 완성

--- wall.py
+++ wall.py
@@ -1,1 +1,28 @@
 from pico2d import *
+
+
+class Wall:
+    def __init__(self, left, top, right, bottom):
+        # 좌상단, 우하단 좌표를 중심점과 크기로 변환
+        self.x = (left + right) // 2
+        self.y = (top + bottom) // 2
+        self.width = right - left
+        self.height = top - bottom
+
+        # 원본 좌표도 저장 (필요시)
+        self.left = left
+        self.top = top
+        self.right = right
+        self.bottom = bottom
+
+    def draw(self):
+        draw_rectangle(*self.get_bb())
+
+    def update(self):
+        pass
+
+    def get_bb(self):
+        return (self.left, self.bottom, self.right, self.top)
+
+    def handle_collision(self, group, other):
+        pass
